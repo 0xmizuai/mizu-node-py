5e5c36fbeae899b9e05f7ac75a46648850c651d5
shudong <dongs2011@gmail.com>
2024-10-15 22:54:26 -0700

    remove R2 constants

--- mizu_node/constants.py
+++ mizu_node/constants.py
@@ -17,12 +17,5 @@ VERIFY_JOB_CALLBACK_URL = os.environ["VERIFY_JOB_CALLBACK_URL"]
 MONGO_URL = os.environ["MONGO_URL"]
 MONGO_DB_NAME = os.environ["MONGO_DB_NAME"]
 
-# r2 for raw data
-ACCOUTN_ID = os.environ["R2_ACCOUNT_ID"]
-R2_ENDPOINT = f"https://{ACCOUTN_ID}.r2.cloudflarestorage.com"
-R2_ACCESS_KEY = os.environ["R2_ACCESS_KEY"]
-R2_SECRET_KEY = os.environ["R2_SECRET_KEY"]
-R2_BUCKET = os.environ["R2_BUCKET"]
-
 # misc
 VERIFICATION_RATIO_BASE = 1000
