26b7409c3118aceef89c20e824c911bfbc23e960
shudong <dongs2011@gmail.com>
2024-11-12 10:27:32 -0800

    return metadata directly

--- data-migrator/src/index.ts
+++ data-migrator/src/index.ts
@@ -21,10 +21,6 @@ function toHex(buffer: ArrayBuffer): string {
 	return [...new Uint8Array(buffer)].map((x) => x.toString(16).padStart(2, '0')).join('');
 }
 
-function epoch(): number {
-	return Math.floor(Date.now() / 1000);
-}
-
 async function md5(data: string): Promise<string> {
 	const encoded = new TextEncoder().encode(data);
 	const myDigest = await crypto.subtle.digest(
@@ -42,20 +38,22 @@ async function save_chunk(r2_key: string, batch: Array<string>, index: number, e
 	const compressed = zlib.deflateSync(content).toString('base64');
 	const metadata = r2_key.split('/');
 	const new_key = r2_key + '/' + index + '.zz';
+	const new_metadata = {
+		batch: metadata[0],
+		decompressed_bytesize: content.length.toString(),
+		bytesize: compressed.length.toString(),
+		chunk: metadata[3].split('.')[0],
+		subchunk: index.toString(),
+		subchunk_size: batch.length.toString(),
+		md5: await md5(compressed),
+		filename: metadata[2],
+		type: metadata[1],
+		created_at: new Date().toISOString(),
+	}
 	await env.MIZU_CMC_V2.put(new_key, compressed, {
-		customMetadata: {
-			batch: metadata[0],
-			decompressed_bytesize: content.length.toString(),
-			bytesize: compressed.length.toString(),
-			chunk: metadata[3].split('.')[0],
-			subchunk: index.toString(),
-			subchunk_size: batch.length.toString(),
-			md5: await md5(compressed),
-			filename: metadata[2],
-			type: metadata[1],
-			created_at: new Date().toISOString(),
-		},
+		customMetadata: new_metadata,
 	});
+	return new_metadata;
 }
 
 export default {
@@ -93,7 +91,7 @@ export default {
 			batches.push(batch_cache);
 		}
 
-		await Promise.all(batches.map((b, index) => save_chunk(r2_key, b, index, env)));
-		return Response.json({ status: 'ok' });
+		const metadata = await Promise.all(batches.map((b, index) => save_chunk(r2_key, b, index, env)));
+		return Response.json({ metadata });
 	},
 } satisfies ExportedHandler<Env>;
