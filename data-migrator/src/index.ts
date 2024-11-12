e20bd05d15ede7ac37ff5fb542f58f60bcaf60d6
shudong <dongs2011@gmail.com>
2024-11-12 10:21:12 -0800

    disable kv

--- data-migrator/src/index.ts
+++ data-migrator/src/index.ts
@@ -1,5 +1,22 @@
 import zlib from 'zlib';
 
+interface Chunk {
+	key: string;
+	compressed: string;
+	metadata: {
+		batch: string;
+		decompressed_bytesize: number;
+		bytesize: number;
+		chunk: string;
+		subchunk: number;
+		subchunk_size: number;
+		md5: string;
+		filename: string;
+		type: string;
+		created_at: number;
+	}
+}
+
 function toHex(buffer: ArrayBuffer): string {
 	return [...new Uint8Array(buffer)].map((x) => x.toString(16).padStart(2, '0')).join('');
 }
@@ -25,19 +42,20 @@ async function save_chunk(r2_key: string, batch: Array<string>, index: number, e
 	const compressed = zlib.deflateSync(content).toString('base64');
 	const metadata = r2_key.split('/');
 	const new_key = r2_key + '/' + index + '.zz';
-	const new_metadata = {
-		batch: metadata[0],
-		decompressed_bytesize: content.length,
-		bytesize: compressed.length,
-		chunk: metadata[3].split('.')[0],
-		subchunk: index,
-		subchunk_size: batch.length,
-		md5: await md5(compressed),
-		filename: metadata[2],
-		type: metadata[1],
-		created_at: epoch(),
-	};
-	await Promise.all([env.MIZU_CMC_V2.put(new_key, compressed), env.KV.put(new_key, JSON.stringify(new_metadata))]);
+	await env.MIZU_CMC_V2.put(new_key, compressed, {
+		customMetadata: {
+			batch: metadata[0],
+			decompressed_bytesize: content.length.toString(),
+			bytesize: compressed.length.toString(),
+			chunk: metadata[3].split('.')[0],
+			subchunk: index.toString(),
+			subchunk_size: batch.length.toString(),
+			md5: await md5(compressed),
+			filename: metadata[2],
+			type: metadata[1],
+			created_at: new Date().toISOString(),
+		},
+	});
 }
 
 export default {
@@ -50,25 +68,13 @@ export default {
 			});
 		}
 
-		const progress = await env.KV.get(r2_key);
-		if (progress !== null) {
-			return Response.json({ status: 'already processed' });
-		}
-
 		const object = await env.MIZU_CMC.get(r2_key);
 		if (object === null) {
 			return new Response(`Not found`, {
 				status: 404,
 			});
 		}
-
-		console.log('======= start processing');
-		console.log("======= r2 object: ", object.size);
-
 		const content: string = zlib.inflateSync(await object.arrayBuffer()).toString();
-		console.log("====== content: ", content.length);
-		console.log("====== content: ", content);
-
 		const batches: Array<Array<string>> = [];
 		let batch_cache: Array<string> = [];
 		let batch_size: number = 0;
@@ -87,32 +93,7 @@ export default {
 			batches.push(batch_cache);
 		}
 
-		console.log("======= batches", batches.length);
-		const totalWrites = Math.floor(batches.length / 3);
-		for (let i = 0; i < totalWrites; i++) {
-			const startIndex = i * 3;
-			await Promise.all([
-				save_chunk(r2_key, batches[startIndex], startIndex, env),
-				save_chunk(r2_key, batches[startIndex + 1], startIndex + 1, env),
-				save_chunk(r2_key, batches[startIndex + 2], startIndex + 2, env),
-			]);
-		}
-		if (batches.length % 3 > 0) {
-			const promises = [];
-			for (let i = totalWrites * 3; i < batches.length; i++) {
-				promises.push(save_chunk(r2_key, batches[i], i, env));
-			}
-			await Promise.all(promises);
-		}
-
-		console.log('======= recording progress');
-		// await env.KV.put(
-		// 	r2_key,
-		// 	JSON.stringify({
-		// 		status: 'processed',
-		// 		processed_at: epoch(),
-		// 	})
-		// );
+		await Promise.all(batches.map((b, index) => save_chunk(r2_key, b, index, env)));
 		return Response.json({ status: 'ok' });
 	},
 } satisfies ExportedHandler<Env>;
