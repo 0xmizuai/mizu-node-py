b7a43d9bcec4a76ea6f5f2cda3bdb83cd98a9e6d
shudong <dongs2011@gmail.com>
2024-11-28 21:01:35 -0800

    fix reward claim logic

--- mizu_node/job_handler.py
+++ mizu_node/job_handler.py
@@ -185,7 +185,7 @@ def handle_finish_job(
 
         if job_result.job_type != JobType.reward and reward_points > 0:
             record_mined_points(rclient, worker, reward_points)
-        if job_result.job_type != JobType.reward:
+        if job_result.job_type == JobType.reward:
             record_reward_claim(rclient, worker, job_result.job_id)
 
         jobs.update_one(
