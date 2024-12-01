6d779f232267889a13525d98e41b133021d3e39f
shudong <dongs2011@gmail.com>
2024-11-30 23:27:37 -0800

    add more global stats

--- mizu_node/job_handler.py
+++ mizu_node/job_handler.py
@@ -21,9 +21,10 @@ from mizu_node.constants import (
 from mizu_node.security import (
     get_lease_ttl,
     record_mined_points,
-    record_reward_claim,
+    reward_claim_event,
     record_reward_event,
     total_mined_points_in_past_n_hour,
+    try_remove_reward_record,
 )
 from mizu_node.types.data_job import (
     BatchClassifyContext,
@@ -183,10 +184,10 @@ def handle_finish_job(
             if settle_reward.token is None:
                 reward_points = float(settle_reward.amount)
 
-        if job_result.job_type != JobType.reward and reward_points > 0:
-            record_mined_points(rclient, worker, reward_points)
         if job_result.job_type == JobType.reward:
-            record_reward_claim(rclient, worker, job_result.job_id)
+            reward_claim_event(rclient, worker, job_result.job_id, parsed.reward_ctx)
+        elif reward_points > 0:
+            record_mined_points(rclient, worker, reward_points)
 
         jobs.update_one(
             {"_id": ObjectId(job_result.job_id)},
@@ -206,7 +207,7 @@ def handle_finish_job(
             job_result.job_type == JobType.reward
             and e.status_code == status.HTTP_404_NOT_FOUND
         ):
-            record_reward_claim(rclient, worker, job_result.job_id)
+            try_remove_reward_record(rclient, worker, job_result.job_id)
         raise e
 
 
--- mizu_node/security.py
+++ mizu_node/security.py
@@ -4,7 +4,7 @@ from fastapi import HTTPException, status
 from pymongo.database import Collection
 from redis import Redis
 
-from mizu_node.common import epoch, is_prod
+from mizu_node.common import epoch
 from mizu_node.constants import (
     ACTIVE_USER_PAST_7D_THRESHOLD,
     COOLDOWN_WORKER_EXPIRE_TTL_SECONDS,
@@ -17,6 +17,7 @@ import jwt
 
 from mizu_node.types.data_job import (
     JobType,
+    RewardContext,
     RewardJobRecord,
     RewardJobRecords,
     WorkerJob,
@@ -82,6 +83,22 @@ def mined_per_day_field(day: int):
     return f"mined_per_day:{day}"
 
 
+def total_mined_points_per_hour_key(hour: int):
+    return f"mined_points:per_hour:{hour}"
+
+
+def total_mined_points_per_day_key(day: int):
+    return f"mined_points:per_day:{day}"
+
+
+def total_rewarded_per_hour_key(token: str, hour: int):
+    return f"rewarded_{token}:per_hour:{hour}"
+
+
+def total_rewarded_per_day_key(token: str, day: int):
+    return f"rewarded_{token}:per_day:{day}"
+
+
 def record_mined_points(rclient: Redis, worker: str, points: float):
     if points > 0.0:
         now = epoch()
@@ -90,6 +107,8 @@ def record_mined_points(rclient: Redis, worker: str, points: float):
         pipeline = rclient.pipeline()
         pipeline.hincrbyfloat(event_name(worker), mined_per_hour_field(hour), points)
         pipeline.hincrbyfloat(event_name(worker), mined_per_day_field(day), points)
+        pipeline.incrbyfloat(total_mined_points_per_hour_key(hour), points)
+        pipeline.incrbyfloat(total_mined_points_per_day_key(day), points)
         pipeline.execute()
 
 
@@ -133,12 +152,32 @@ def record_reward_event(rclient: Redis, worker: str, job: WorkerJob):
     )
 
 
-def record_reward_claim(rclient: Redis, worker: str, job_id: str):
+def try_remove_reward_record(rclient: Redis, worker: str, job_id: str):
     rewards = get_valid_rewards(rclient, worker)
     filtered = [r for r in rewards.jobs if r.job_id != job_id]
-    if len(filtered) != len(rewards.jobs):
+    if len(filtered) < len(rewards.jobs):
         rewards.jobs = filtered
         rclient.hset(event_name(worker), REWARD_FIELD, rewards.model_dump_json())
+
+
+def get_token_name(ctx: RewardContext) -> str:
+    return "points" if ctx.token is None else "usdt"
+
+
+def reward_claim_event(rclient: Redis, worker: str, job_id: str, ctx: RewardContext):
+    rewards = get_valid_rewards(rclient, worker)
+    rewards.jobs = [r for r in rewards.jobs if r.job_id != job_id]
+    now = epoch()
+    hour = now // 3600
+    day = now // 86400
+    token_name = get_token_name(ctx)
+    pipeline = rclient.pipeline()
+    pipeline.incrbyfloat(
+        total_rewarded_per_hour_key(token_name, hour), float(ctx.amount)
+    )
+    pipeline.incrbyfloat(total_rewarded_per_day_key(token_name, day), float(ctx.amount))
+    pipeline.hset(event_name(worker), REWARD_FIELD, rewards.model_dump_json())
+    pipeline.execute()
 
 
 def validate_worker(r_client: Redis, worker: str, job_type: JobType) -> bool:
