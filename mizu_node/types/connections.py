0bdd0e1113780e16b55ea29306bc05539adf38cd
shudong <dongs2011@gmail.com>
2024-12-12 14:45:03 -0800

    make query db url as optional

--- mizu_node/types/connections.py
+++ mizu_node/types/connections.py
@@ -13,9 +13,17 @@ class Connections:
         query_db_url: str | None = None,
         redis: AsyncRedis | None = None,
     ):
-        self.job_db_url = job_db_url or os.environ["JOB_DB_URL"].replace(
-            "postgresql://", "postgresql+asyncpg://"
+        self.redis = redis or AsyncRedis.from_url(
+            os.environ["REDIS_URL"], decode_responses=True
         )
+        logging.info(f"Connected to redis at {os.environ['REDIS_URL']}")
+
+        job_db_url = (
+            job_db_url
+            or os.environ.get("JOB_DB_URL", None)
+            or os.environ["POSTGRES_URL"]
+        )
+        self.job_db_url = job_db_url.replace("postgresql://", "postgresql+asyncpg://")
         logging.info(f"Connecting to postgres at {self.job_db_url}")
 
         self.job_db_engine = create_async_engine(self.job_db_url, echo=False)
@@ -23,19 +31,19 @@ class Connections:
             self.job_db_engine, class_=AsyncSession, expire_on_commit=False
         )
 
-        self.query_db_url = query_db_url or os.environ["QUERY_DB_URL"].replace(
-            "postgresql://", "postgresql+asyncpg://"
-        )
-        logging.info(f"Connecting to postgres at {self.query_db_url}")
-        self.query_db_engine = create_async_engine(self.query_db_url, echo=False)
-        self.query_db_session = sessionmaker(
-            self.query_db_engine, class_=AsyncSession, expire_on_commit=False
-        )
-
-        self.redis = redis or AsyncRedis.from_url(
-            os.environ["REDIS_URL"], decode_responses=True
-        )
-        logging.info(f"Connected to redis at {os.environ['REDIS_URL']}")
+        query_db_url = query_db_url or os.environ.get("QUERY_DB_URL", None)
+        if query_db_url:
+            self.query_db_url = query_db_url.replace(
+                "postgresql://", "postgresql+asyncpg://"
+            )
+            logging.info(f"Connecting to postgres at {self.query_db_url}")
+            self.query_db_engine = create_async_engine(self.query_db_url, echo=False)
+            self.query_db_session = sessionmaker(
+                self.query_db_engine, class_=AsyncSession, expire_on_commit=False
+            )
+        else:
+            self.query_db_engine = None
+            self.query_db_session = None
 
     @asynccontextmanager
     async def get_job_db_session(self):
@@ -49,6 +57,9 @@ class Connections:
 
     @asynccontextmanager
     async def get_query_db_session(self):
+        if not self.query_db_session:
+            raise Exception("Query database is not initialized")
+
         async with self.query_db_session() as session:
             try:
                 yield session
@@ -59,5 +70,6 @@ class Connections:
 
     async def close(self):
         await self.job_db_engine.dispose()
-        await self.query_db_engine.dispose()
+        if self.query_db_engine:
+            await self.query_db_engine.dispose()
         await self.redis.aclose()
